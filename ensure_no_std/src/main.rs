461f68afedcc1e6811a276b4d9b581e312547182
Warlock <internalmike@gmail.com>
2022-06-22 23:18:32 +0300

    ens

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -6,7 +6,7 @@
 
 use core::panic::PanicInfo;
 use exit_no_std::exit;
-use timer_no_std::{get_monotonic_milliseconds, sleep_milliseconds};
+use timer_no_std::{MonoTime, sleep_ms_u8};
 
 #[cfg(windows)]
 #[link(name="msvcrt")]
@@ -19,7 +19,7 @@ pub extern fn panic(_info: &PanicInfo) -> ! {
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let _ = get_monotonic_milliseconds();
-    sleep_milliseconds(100);
+    let _ = MonoTime::get();
+    sleep_ms_u8(100);
     exit(0);
 }
